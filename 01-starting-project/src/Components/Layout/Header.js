c75684045ed726a84be093f76af9561a4fd1d600
amberlee <amberleehuynh5@gmail.com>
2021-11-15 19:34:11 -0800

    Commit

---
+++ 01-starting-project/src/Components/Layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+const Header = props => {
+  return <Fragment>
+    <header>
+      <h1>Meals</h1>
+      <button>Cart</button>
+    </header>
+    <div>
+      <img></img>
+    </div>
+  </Fragment>
+};
+
+export default Header;
